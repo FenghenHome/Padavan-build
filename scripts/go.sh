614b34e79ec356b39a81830b8f1c885ad3cfc01a
FenghenHome <FenghenHome@users.noreply.github.com>
2021-04-02 14:57:06 +0800

    Update go.sh

--- scripts/go.sh
+++ scripts/go.sh
@@ -1,16 +1,19 @@
-#!/bin/bash
-GO_DIR="/opt/hostedtoolcache/go"
-GO_VER="/opt/go_ver.txt"
-: >$GO_VER
-
-dir=$(ls -l "$GO_DIR" | sed '{1!G;h;$!d}' | awk '/^d/ {print $NF}')
-for i in $dir; do
-  echo $i
-  echo $i >>$GO_VER
+#!/bin/sh
+goCacheRootDir="/opt/hostedtoolcache/go"
+goVersions="/opt/goVersions.txt"
+:> $goVersions
+chmod 777 $goVersions
+dir=$(ls -l "$goCacheRootDir" | sed '{1!G;h;$!d}' | awk '/^d/ {print $NF}')
+for i in $dir
+do
+echo $i
+echo $i >> $goVersions
 done
-new_ver=$(sed -n '1p' $GO_VER)
-old_ver=$(sed -n '2p' $GO_VER)
-echo "rm -rf $GO_DIR/$old_ver"
-rm -rf $GO_DIR/$old_ver
-echo "mv $GO_DIR/$new_ver $GO_DIR/$old_ver"
-mv $GO_DIR/$new_ver $GO_DIR/$old_ver
+new_ver=`sed -n '1p' $goVersions`
+old_ver=`sed -n '2p' $goVersions`
+echo "rm -rf $goCacheRootDir/$old_ver"
+rm -rf $goCacheRootDir/$old_ver
+#echo "mv $goCacheRootDir/$new_ver $goCacheRootDir/$old_ver"
+#mv $goCacheRootDir/$new_ver $goCacheRootDir/$old_ver
+echo "cp -rf $goCacheRootDir/$new_ver $goCacheRootDir/$old_ver"
+cp -rf $goCacheRootDir/$new_ver $goCacheRootDir/$old_ver
